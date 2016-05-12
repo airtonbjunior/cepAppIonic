6958395ead14a585055a74d02020bbfed7c38c46
Airton Jr <airtonbjunior@gmail.com>
2016-05-12 17:58:51 -0300

    Add Android Platform, form validation, toggle answer div

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,33 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
 .controller('webServerCtrl', function($scope, $http) {
 
-  $scope.isHidden = true; // default hidden
+  $scope.isHidden      = true; // default hidden
+  $scope.isHiddenError = true;
 
-  $scope.showHidden = function () {
-    $scope.isHidden = false;
+  $scope.showHidden = function (error) {
+    if (error) { 
+      $scope.isHiddenError = false; 
+      $scope.isHidden = true; 
+    } else { 
+      $scope.isHidden = false;
+      $scope.isHiddenError = true;  
+    }
   }
 
+
   $scope.getData = function (cepParam) {
-    $http.get("http://correiosapi.apphb.com/cep/" + cepParam)
+    
+    var cepParamFunction = String(cepParam).replace("-", "");
+
+    var regExp = /[0-9]{8}/;
+
+    if(! regExp.test(cepParamFunction)) {
+      alert("Verifique o formato do CEP!");
+      return;
+    }
+
+    $http.get("http://correiosapi.apphb.com/cep/" + cepParamFunction)
       .success(function(data) {
         console.log(data);
         $scope.bairro           = data.bairro;
@@ -24,10 +37,10 @@ angular.module('starter', ['ionic'])
         $scope.estado           = data.estado;
         $scope.cep              = data.cep;
 
-        $scope.showHidden();
+        $scope.showHidden(false);
       })
       .error(function(data){
-        alert("não deu certo");
+        $scope.showHidden(true);
       })
   }
 })
@@ -35,13 +48,7 @@ angular.module('starter', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
